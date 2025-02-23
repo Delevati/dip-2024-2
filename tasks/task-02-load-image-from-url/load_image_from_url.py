bd908c42c46521de90a1e867a8513439df319ec1
Luryan Delevati Dorneles <luryand@MacBook-Air-de-Luryan.local>
2025-02-23 15:34:21 -0300

    [check]2, 3, 4

--- tasks/task-02-load-image-from-url/load_image_from_url.py
+++ tasks/task-02-load-image-from-url/load_image_from_url.py
@@ -1,8 +1,11 @@
 import argparse
 import numpy as np
 import cv2 as cv
+# Tive que adicionar request, observei que ela está inclusa no requirements, tentei algumas alternativas como o 
+# cv.videoCapture, e o cv.imdecode diretamente mas sem êxito, então resolvi adicionar a request para finalizar o trabalho.
+import requests
 
-def load_image_from_url(url, **kwargs):
+def load_image_from_url(url=None, **kwargs):
     """
     Loads an image from an Internet URL with optional arguments for OpenCV's cv.imdecode.
     
@@ -15,9 +18,35 @@ def load_image_from_url(url, **kwargs):
     """
     
     ### START CODE HERE ###
-    ### TODO
+    # python3 load_image_from_url.py "https://tm.ibxk.com.br/2012/3/materias/529462367269129.jpg"
+    if url is None:
+        # Argparse to get url
+        parser = argparse.ArgumentParser(description="Load an image from a URL")
+        parser.add_argument('url', type=str, help="URL of the image")
+        args = parser.parse_args()
+
+        url = args.url
+
+        # Define grayscale flag
+        flags = cv.IMREAD_GRAYSCALE
+
+    # Getting image from URL
+    response = requests.get(url)
+    
+    # Convert content to array uint 8
+    image_array = np.frombuffer(response.content, dtype=np.uint8)
+
+    # Decode image with cv2.imdecode
+    image = cv.imdecode(image_array, **kwargs, flags=flags)
+    
+    if image is None:
+        raise ValueError("Failed: empty image")
+    
+    print("Array:")
+    print(image)
     ### END CODE HERE ###
     
     return image
 
 load_image_from_url()
+# load_image_from_url()
--- tasks/task-03-get-image-info/task-03-get_image_info.py
+++ tasks/task-03-get-image-info/task-03-get_image_info.py
@@ -12,7 +12,16 @@ def get_image_info(image):
     """
     
     ### START CODE HERE ###
-    ### TODO
+    # take image dimensions 
+    height, width = image.shape[:2]
+    dtype = str(image.dtype)
+    depth = 1 if len(image.shape) == 2 else image.shape[2]
+    
+    #min - max - mean - std
+    min_val = float(np.min(image))
+    max_val = float(np.max(image))
+    mean_val = float(np.mean(image))
+    std_val = float(np.std(image))
     ### END CODE HERE ###
 
     return {
